355a4ffeae67e567675c24bc7d2c63ff66fbcf94
blackwell <blackwellsmithiv@gmail.com>
2019-02-19 15:43:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,9 @@ require "pry"
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
-
+html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   projects = {}
