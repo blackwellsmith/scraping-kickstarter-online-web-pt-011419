19ee7ac20168c98c6896b82e74769b28d01019da
blackwell <blackwellsmithiv@gmail.com>
2019-02-19 15:39:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,7 @@
 require "nokogiri" # require libraries/modules here
 require "pry"
+
+
 def create_project_hash
   
   html = File.read('fixtures/kickstarter.html')
@@ -9,9 +11,3 @@ binding.pry
 end
 create_project_hash 
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
